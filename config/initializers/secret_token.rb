89a0aefa12448b62d44883e25be27e53af00eedd
Elizabeth Weiss <elizabethjweiss@gmail.com>
2013-03-03 02:48:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoiffedGiraffe::Application.config.secret_token = 'cbe9ca35503a56e443c2511b725d540631e6c534540b6bdcdb27311ba0e1748ebbb3fbb3c55d0b6c568171ee2f0f78b3f9aa5ef855883d9dcc3504caa576593b'
